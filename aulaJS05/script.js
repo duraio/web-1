8c259ffdfcdcdc1220199bf871ce3dd7b87bfeb4
duraio <viniciusmoreiras@unipam.edu.br>
2025-06-05 01:15:46 +0000

    calculadora

---
+++ aulaJS05/script.js
@@ -0,0 +1,15 @@
+function Soma(){
+   result.value = parseInt(v1.value) + parseInt(v2.value);
+}
+
+function Diminuir(){
+    result.value = parseInt(v1.value) - parseInt(v2.value);
+}
+
+function Multiplicar(){
+    result.value = parseInt(v1.value) * parseInt(v2.value);
+}
+
+function Divisao(){
+    result.value = parseInt(v1.value) / parseInt(v2.value);
+}
